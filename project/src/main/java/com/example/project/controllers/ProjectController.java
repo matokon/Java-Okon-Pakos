9700d031375f9bd81884dee1d65ccecdf3d05e6a
Mateusz Okon <mateusz.okon04@gmail.com>
2026-03-25 21:48:13 +0100

    feat: added two endpoints(GET/POST)

---
+++ project/src/main/java/com/example/project/controllers/ProjectController.java
@@ -0,0 +1,27 @@
+package com.example.project.controller;
+
+import com.example.project.entity.Project;
+import com.example.project.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+    private final ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @PostMapping
+    public Project addProject(@RequestBody Project project) {
+        return projectRepository.save(project);
+    }
+}
