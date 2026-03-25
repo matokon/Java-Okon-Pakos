8c6f53d3dbd0754481a08417a29347749339a955
Frenky777 <89804963+Frenky777@users.noreply.github.com>
2026-03-25 12:11:16 +0100

    Update ProjectApplication.java
    typo, it doesn't compile because of that

--- project/src/main/java/com/example/project/ProjectApplication.java
+++ project/src/main/java/com/example/project/ProjectApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpriAngApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
